b95d11934a6c9ea4e62c490050cdc4da41e3f20b
Tarek Marjieh <marjieh.tarek.98@gmail.com>
2025-03-28 18:44:16 +0300

    Initial commit

---
+++ src/main/my_salary/PopcornPalaceApplication.java
@@ -0,0 +1,13 @@
+package com.mysalary.my_salary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySalaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySalaryApplication.class, args);
+	}
+
+}
